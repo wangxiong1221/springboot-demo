6dbbbfdfb61e19a7a5758c177192160608091aa1
wangxiong1221 <73984942+wangxiong1221@users.noreply.github.com>
2021-12-28 21:05:26 +0800

    Update SpringbootDemoApplication.java

--- src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
@@ -9,7 +9,7 @@ public class SpringbootDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
         System.out.println("测试提交github");
-
+        System.out.println("哈哈，我修改了github上的代码");
     }
 
 }
